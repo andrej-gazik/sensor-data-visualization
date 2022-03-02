433cb9223cedb00c24da18c090849286ff669de8
ajogod <gazikandrej0@gmail.com>
2022-03-02 18:20:57 +0100

    Created app api and models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import Visualization, Room, Sensor, SensorData, File
 
 # Register your models here.
+admin.site.register(Visualization)
+admin.site.register(Room)
+admin.site.register(Sensor)
+admin.site.register(File)
+admin.site.register(SensorData)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Visualization(models.Model):
-    
+
+    STATUS = (
+        ('blank', 'Blank'),
+        ('room', 'Room'),
+        ('file', 'File'),
+        ('sensors', 'Sensors'),
+        ('available', 'Available'),
+    )
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(
+        max_length = 10,
+        choices = STATUS,
+        default = 'blank',
+        )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Room(models.Model):
+    visualization_id = models.ForeignKey(Visualization, on_delete=models.CASCADE)
+    x0 = models.PositiveIntegerField()
+    y0 = models.PositiveIntegerField()
+    x1 = models.PositiveIntegerField()
+    y1 = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Sensor(models.Model):
+    visualization_id = models.ForeignKey(Visualization, on_delete=models.CASCADE)
+    name = models.PositiveSmallIntegerField()
+    x = models.PositiveIntegerField()
+    y = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class File(models.Model):
+    visualization_id = models.ForeignKey(Visualization, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='data/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class SensorData(models.Model):
+    visualization_id = models.ForeignKey(Visualization, on_delete=models.CASCADE)
+    sensor_name = models.PositiveIntegerField()
+    value = models.FloatField(null=True)
+    measurement_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
