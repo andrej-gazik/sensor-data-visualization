a5db7414871be04519c9616f9e40de84cbe2b7be
ajogod <gazikandrej0@gmail.com>
2022-05-16 06:04:25 +0200

    Working dockerization

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,28 +81,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'database',
-#         'PORT': 5432,
-#     }
-#}
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'NAME': 'sensordatavizualization',
-       'USER': 'postgres',
-       'PASSWORD': 'ajopajo',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
 }
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ #      'NAME': 'sensordatavizualization',
+#       'USER': 'postgres',
+#       'PASSWORD': 'ajopajo',
+#      'HOST': 'localhost',
+#       'PORT': '5432',
+#   }
+#}
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
